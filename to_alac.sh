7a0b1735f3fc102308bc7d69b5c7cc4e49b804e1
Piotr Palka <piotr.palka123@gmail.com>
2019-04-12 23:07:37 +1000

    Added ability to provide multiple files as arguments at once

--- to_alac.sh
+++ to_alac.sh
@@ -10,28 +10,58 @@
 # - mp4v2
 #
 
-filename="$1"
-alac_filename="${filename%.*}.m4a"
-ffmpeg -y -i "$filename" -acodec alac -vn "$alac_filename"
+usage() {
+  echo "Script to convert audio files to alac (m4a container)."
+  echo
+  echo "Usage: $(basename $0) audio_file_names"
+  echo
 
-pic_name="cover.mjpeg"
-ffmpeg -i "$filename" -map 0:$( \
-  ffprobe \
-    -loglevel quiet \
-    -print_format json \
-    -hide_banner \
-    -select_streams v \
-    -show_streams "$filename" | jq '.streams[] | if .tags.comment == "Cover (front)" then .index else empty end') "$pic_name" || if [ -e "$alac_filename" ]; then
-      # if alac file exists we can safey remove source file and finish the script
-      echo "No cover art I guess"
-      rm "$filename"
-      exit 0
-    fi
+  exit 0
+}
 
-# If cover art was found apply it to alac file
-mp4art --add "$pic_name" "$alac_filename"
-rm "$pic_name"
-# Remove source file if dest file exists
-if [ -e "$alac_filename" ]; then
-  rm "$filename"
+# Help handle
+while getopts "h" optname; do
+  case "$optname" in
+    "h")
+      usage
+    ;;
+  esac
+done
+
+# The argument must be provided
+if [ $# -lt 1 ]; then
+  echo
+  echo "Provide at least one file name"
+  echo
+  usage
 fi
+
+# Conversion loop
+for filename in "$@"; do
+
+  alac_filename="${filename%.*}.m4a"
+  ffmpeg -y -i "$filename" -acodec alac -vn "$alac_filename"
+
+  pic_name="cover.mjpeg"
+  ffmpeg -i "$filename" -map 0:$( \
+    ffprobe \
+      -loglevel quiet \
+      -print_format json \
+      -hide_banner \
+      -select_streams v \
+      -show_streams "$filename" | jq '.streams[] | if .tags.comment == "Cover (front)" then .index else empty end') "$pic_name" || if [ -e "$alac_filename" ]; then
+        # if alac file exists we can safey remove source file and finish the script
+        echo "No cover art I guess"
+        rm "$filename"
+        exit 0
+      fi
+
+  # If cover art was found apply it to alac file
+  mp4art --add "$pic_name" "$alac_filename"
+  rm "$pic_name"
+  # Remove source file if dest file exists
+  if [ -e "$alac_filename" ]; then
+    rm "$filename"
+  fi
+
+done
